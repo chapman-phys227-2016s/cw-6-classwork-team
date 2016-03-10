8c312900fc2e40c15e19324fae0b21aced413ef6
Austin Ayers <ayers111@mail.chapman.edu>
2016-03-10 18:13:57 +0000

    Added py file

---
+++ nonhomogenous_linear_ODE.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+"""
+File: adaptive_trapezint.py
+Copyright (c) 2016 Austin Ayers
+License: MIT
+
+Course: PHYS227
+Assignment: C.1
+Date: Feb 11, 2016
+Email: ayers111@mail.chapman.edu
+Name: Austin Ayers
+Description: Implements Euler's method
+"""
